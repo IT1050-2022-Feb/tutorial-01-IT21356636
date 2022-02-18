c634de64204e9298c4f682539db209fa20bf8253
Kavindu Lakshan <99538119+IT21356636@users.noreply.github.com>
2022-02-18 14:25:36 +0000

    add if else statement

--- Tute04.c
+++ Tute04.c
@@ -3,8 +3,11 @@
 Implement the three functions minimum(), maximum() and multiply() below the main() function.
 
 Do not change the code given in the main() function when you are implementing your solution.*/
+ #include <stdio.h>
 
-#include <stdio.h>
+int 
+int maximum (int no1,int no2);
+int multiply (int no1,int no2);
 
 int main() {
    int no1, no2;
@@ -16,4 +19,32 @@ int main() {
    printf("%d ", maximum(no1, no2));
    printf("%d ", multiply(no1, no2));
    return 0;
+}
+int minimum(no1,no2)
+{
+  if(no1 <= no2);
+  {
+    return no1;
+  }
+  else
+  {
+    return no2;
+  }
+}
+
+int maximum(no1,no2)
+{
+  {
+    return no1;
+  }
+  else
+  {
+    return no2;
+  }
+}
+
+int multiply(no1,no2)
+{
+  int multiply = no1 * no2;
+  return multiply;
 }
